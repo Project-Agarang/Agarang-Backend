769c71c165705f9c12ba71633531e7b7b21376da
JinGoo <zzgg3040@naver.com>
2024-09-24 19:02:54 +0900

    first commit

---
+++ src/test/java/org/ku/agarangproject/AgarangProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ku.agarangproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgarangProjectApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
